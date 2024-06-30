4b85cade9aea2cc0ad974e747ae92ec3f89f3b77
burningmannn <daniil24022002@gmail.com>
2024-06-30 12:52:29 +0500

    Create API

---
+++ APP/views.py
@@ -0,0 +1,26 @@
+from django.http import JsonResponse
+from .models import Song
+from .serializers import AlbumSerializer, SongSerializer
+
+
+def get_song(request):
+    songs = Song.objects.select_related('album').all()
+    albums = {}
+    favorites = {'name': 'Favorites', 'image': 'favorite', 'URL_image': '', 'songs': []}
+
+    for song in songs:
+        album_name = song.album.name
+        album_image = song.album.image
+        album_url_image = song.album.URL_image
+
+        if album_name not in albums:
+            albums[album_name] = {'name': album_name, 'image': album_image, 'URL_image': album_url_image, 'songs': []}
+
+        albums[album_name]['songs'].append(SongSerializer(song).data)
+
+        if song.favorite:
+            favorites['songs'].append(SongSerializer(song).data)
+
+    albums['Favorites'] = favorites
+    serializer = AlbumSerializer(albums.values(), many=True)
+    return JsonResponse(serializer.data, safe=False)
